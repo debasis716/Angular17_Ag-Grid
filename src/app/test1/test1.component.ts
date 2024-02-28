ddfe0b508cdc35992f377f5360fb5173257d5ff7
Debasis716 <debasis716@gmail.com>
2024-02-28 15:32:26 +0530

    skip test and css file

---
+++ src/app/test1/test1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test1',
+  standalone: true,
+  imports: [],
+  templateUrl: './test1.component.html'
+})
+export class Test1Component {
+
+}
